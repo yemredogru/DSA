43592b81de4be04456b84b59e58748f932b5bf53
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2025-10-26 11:09:09 +0100

    Create rotate-list.js

---
+++ linkedlist/rotate-list.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if (!head || !head.next || k === 0) return head;
+    
+    var length = 1;
+    var tail = head;
+    
+    while(tail.next) {
+        tail = tail.next;
+        length += 1;
+    }    
+    k = k % length;
+    if (k === 0) return head;
+    
+    var newTail = head;
+    var stepsToNewTail = length - k - 1;
+    
+    for (var i = 0; i < stepsToNewTail; i++) {
+        newTail = newTail.next;
+    }
+    
+    var newHead = newTail.next;
+    
+    newTail.next = null;   
+    tail.next = head;
+    
+    return newHead;
+};
